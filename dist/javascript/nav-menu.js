e13a803bacfeb65f5efee964593892c19c643c41
Matt Kissell <kissellmj15@gmail.com>
2020-04-23 20:43:19 +0100

    MK Navigation overlay javascript for mobile

---
+++ dist/javascript/nav-menu.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mobileNavigation").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mobileNavigation").style.width = "0%";
+}
